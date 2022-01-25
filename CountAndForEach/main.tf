a07e9d6d82014a4736675a3d1e02916f0875d85d
hyun6ik <fkanwm@naver.com>
2022-01-25 13:12:53 +0900

    Add #10 CountAndForEach - ForEach

--- CountAndForEach/main.tf
+++ CountAndForEach/main.tf
@@ -36,3 +36,51 @@ resource "aws_iam_user" "count" {
 output "count_user_arns" {
   value = aws_iam_user.count.*.arn
 }
+
+/*
+* for_each
+* set = List Unique Element
+* map = Key Value
+*/
+
+# for_each를 사용하면 블록안에서
+# each.key와 each.value를 사용할 수 있다.
+# set = each.key와 each.value가 같다.
+# map = each.key, each.value가 key value와 같다.
+resource "aws_iam_user" "for_each_set" {
+  for_each = toset([
+    "for-each-set-user-1",
+    "for-each-set-user-2",
+    "for-each-set-user-3",
+  ])
+
+  name = each.key
+}
+
+output "for_each_set_user_arns" {
+  value = values(aws_iam_user.for_each_set).*.arn
+}
+
+resource "aws_iam_user" "for_each_map" {
+  for_each = {
+    hyun6ik = {
+      level = "high"
+      manager = "hyun6ikManager"
+    }
+    bob = {
+      level = "mid"
+      manager = "hyun6ikManager"
+    }
+    john = {
+      level = "low"
+      manager = "steve"
+    }
+  }
+
+  name = each.key
+  tags = each.value
+}
+
+output "for_each_map_user_arns" {
+  value = values(aws_iam_user.for_each_map).*.arn
+}
